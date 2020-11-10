05ce2a209785c819f8f2e5c0b29853f9e92ff9c6
wudibin <781721794@qq.com>
2020-11-11 00:41:39 +0800

    [wu] font : finish modules-classes-and-async homework

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,18 @@
-export const getPoetry
+export const getPoetry = () => {
+    return fetch('https://v1.jinrishici.com/all.json',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => response.json())
+        .then((result) => {
+            let res = [];
+            res.push(result.origin);
+            res.push(result.author);
+            res.push(result.content);
+            return res;
+        })
+        .catch(result => {
+            console.log(result);
+        });
+};
